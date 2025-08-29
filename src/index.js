088c334aadd42933dee286a2f928f9190487f1ca
Theoadal <theoadal@gmail.com>
2025-08-29 18:08:26 +0300

    Temp rename Redux -> Redux_temp

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import "./index.css";
 import App from "./App";
 
 import { Provider } from "react-redux";
-// import store from "./Redux/store";
+// import store from "./Redux/store.js";
 
-import { store, persistor } from "./Redux/store";
+import { store, persistor } from "./Redux/store.js";
 import { PersistGate } from "redux-persist/integration/react";
 
 import reportWebVitals from "./reportWebVitals";
