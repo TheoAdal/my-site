966340aa6a3b50b6e8690b1f06c76c0cec6b052c
Theoadal <theoadal@gmail.com>
2025-09-02 23:44:38 +0300

    cant fix translation problem on NotFound

--- src/components/ErrorPage/NotFound.js
+++ src/components/ErrorPage/NotFound.js
@@ -1,14 +1,19 @@
 import React from "react";
 
-import { useTranslation } from "react-i18next";
+// import { useTranslation } from "react-i18next";
 
 const NotFound = () => {
-  const { t } = useTranslation();
+  // const { t } = useTranslation();
 
   return (
     <div className="not-found">
-      <h1>{t("error.title")}</h1> 
-      <p>{t("error.text")}</p>
+      {/* <h1>{t("error.title")}</h1> 
+      <p>{t("error.text")}</p> */}
+      <h1>"Oops! That page can’t be found."</h1>
+      <p>
+        The page you are looking for might have been removed, had its name
+        changed or is temporarily unavailable.
+      </p>
     </div>
   );
 };
