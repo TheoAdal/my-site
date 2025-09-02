8cf0eb1eeb38a554353d78d83b6ace8e99220632
Theoadal <theoadal@gmail.com>
2025-09-02 23:41:02 +0300

    fix

--- src/components/ErrorPage/NotFound.js
+++ src/components/ErrorPage/NotFound.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 
 import { useTranslation } from "react-i18next";
 
 const NotFound = () => {
-  const { t } = useTranslation(); 
+  const { t } = useTranslation();
 
   return (
     <div className="not-found">
