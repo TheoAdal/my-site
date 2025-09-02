cee32ff98766fb92b3ea0fc495823d8f32135150
Theoadal <theoadal@gmail.com>
2025-09-02 23:11:35 +0300

    error page translation

--- src/components/ErrorPage/NotFound.js
+++ src/components/ErrorPage/NotFound.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import { useTranslation } from "react-i18next";
+
 const NotFound = () => {
+  const { t } = useTranslation(); 
+
   return (
     <div className="not-found">
-      <h1>Oops! That page can’t be found.</h1>
-      <p>The page you are looking for might have been removed, had its name changed or is temporarily unavailable.</p>
+      <h1>{t("error.title")}</h1>
+      <p>{t("error.text")}</p>
     </div>
   );
 };
