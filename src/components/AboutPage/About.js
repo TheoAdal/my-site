10c48c44796d55aaeef0caf2839b501d5d656f6c
Theoadal <theoadal@gmail.com>
2025-09-02 17:07:39 +0300

    css changes, need to fix the pixel perfect problem

--- src/components/AboutPage/About.js
+++ src/components/AboutPage/About.js
@@ -1,39 +1,125 @@
 import React from "react";
 import "./About.css"; // import styles
 
-import photoprofile from "../../images/profilephoto.jpg"
+import photoprofile from "../../images/profilephoto.jpg";
+
+// import usePixelPerfectContainers from "../../utilities/usePixelPerfectContainers";
 
 function About() {
+  // usePixelPerfectContainers();
   return (
-    <div className="nes-container is-rounded is-dark about-container">
-      {/* Left side - text */}
-      <div className="about-text">
-        <h1 className="nes-text is-success">Hi there, I'm Theo!</h1>
-        <p>
-          Just another CS-related graduate trying his absolute best. My journey
-          in FullStack Development began roughly in 2023 through academic
-          projects and has evolved into something I am really proud of.
-        </p>
-        <p>
-          When I am not coding, you will probably find me on some camping site
-          or a hopeless indie bar. My friends call me a chameleon when it comes
-          to my entertainment.
-        </p>
-        <p>
-          My goal is to broaden my curiosity about new technologies and never,
-          ever settle down, always pushing myself to learn, build and explore
-          the unknown!
-        </p>
+    <div className="about-wrap">
+      <div className="nes-container is-rounded is-dark about-container pixel-perfect">
+        {/* OPENING */}
+        <div className="about-text">
+          <h1 className="nes-text is-primary">Hi there, I'm Theo!</h1>
+          <p>
+            Just another CS-related graduate trying his absolute best. My
+            journey in FullStack Development began roughly in 2023 through
+            academic projects and has evolved into something I am really proud
+            of.
+          </p>
+          <p>
+            When I am not coding, you will probably find me on some camping site
+            or a hopeless indie bar. My friends call me a chameleon when it
+            comes to my entertainment.
+          </p>
+          <p>
+            My goal is to broaden my curiosity about new technologies and never,
+            ever settle down, always pushing myself to learn, build and explore
+            the unknown!
+          </p>
+        </div>
+        {/* AVATAR */}
+        <div className="about-photo">
+          <img src={photoprofile} alt="" className="avatar" />
+        </div>
       </div>
-
-      {/* Right side - avatar */}
-      <div className="about-photo">
-        {/* Replace src with your image later */}
-        <img
-          src={photoprofile}
-          alt=""
-          className="avatar"
-        />
+      <div className="nes-container is-rounded is-dark education-container pixel-perfect">
+        {/* EDUCATION */}
+        <h2 className="nes-text is-primary education-title">Education</h2>
+        <div className="education-text">
+          <h3 className="nes-text is-success diploma-title">
+            Computer Engineering & Informatics
+          </h3>
+          <h3 className="nes-text is-error university-name">
+            Cyprus University of Technology
+          </h3>
+          <ul className="nes-list is-circle">
+            <li>
+              <i className="nes-logo"></i>
+              <strong> PHP,SQL:</strong> Collaborated with a team of five
+              students to develop an eCommerce website for the company “TUFIT
+              CYPRUS” and a “Clock In” application for the university, deisgned
+              to track employee attendance.
+            </li>
+            <li>
+              <i className="nes-logo"></i>
+              <strong> Thesis:</strong> Developed an aplication aimed at
+              combating loneliness among the elderly by matching with volunteers
+              through the platform, offering social interaction and
+              companionship. Built using ReactJs for the front-end, NodeJs &
+              ExpressJs for the backend and MongoDB for the database management.
+              The live site can be accessed at{" "}
+              <a
+                href="https://graduateproject-8f797.web.app"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="nes-text is-primary"
+              >
+                https://graduateproject-8f797.web.app
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="nes-container is-rounded is-dark experience-container">
+        <h2 className="nes-text is-primary education-title ">
+          Work Experience
+        </h2>
+        <div className="experience-text">
+          <h3 className="nes-text is-success job-title">Lab Assistant</h3>
+          <h3 className="nes-text is-error company-name">
+            OneLearn Global Services Ltd
+          </h3>
+          <ul className="nes-list is-circle">
+            <li>
+              <i className="nes-logo"></i>
+              <strong> </strong>
+              Responsible for copying data from a server to over 30 company made
+              mini PC by the name "Enza" for training purposes.{" "}
+            </li>
+            <li>
+              <i className="nes-logo"></i>
+              <strong> </strong>
+              Responsible for assemblying hardware on a "Enza" as well as
+              installing OS and Microsoft Office apps depending on the client's
+              order.
+            </li>
+          </ul>
+        </div>
+        <div className="experience-text">
+          <h3 className="nes-text is-success job-title">
+            Automation & Installation Assistant
+          </h3>
+          <h3 className="nes-text is-error company-name">
+            Piperaris Security Cyprus
+          </h3>
+          <ul className="nes-list is-circle">
+            <li>
+              <i className="nes-logo"></i>
+              <strong> </strong>
+              Participated in KNX training sessions and gained hands-on
+              experience with automation tools.
+            </li>
+            <li>
+              <i className="nes-logo"></i>
+              <strong> </strong>
+              Configured, installed, and tested Electron switches, ensuring
+              their proper functionality.
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
--- src/components/HomePage/Home.js
+++ src/components/HomePage/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Home.css";
 
+import photoprofile from "../../images/profilephoto.jpg"
+
 function Home() {
   return (//<div className="nes-container is-centered with-title" style={{ margin: "2rem" }}>
-    <div>
+    <div className="home">
       {/* <p className="title">Title Text</p> */}
-      <h1 className="nes-text is-primary">Currently in local deployment</h1>
+      <h2 className="nes-text is-primary">Currently in local deployment</h2>
       <div className="page-wrap">
         <div className="nes-container is-dark is-centered nes-card"> 
           <p>
@@ -14,6 +16,37 @@ function Home() {
           </p>
         </div>
       </div>
+      <div className="nes-container is-rounded is-dark about-container">
+      {/* Left side - text */}
+      <div className="about-text">
+        <h1 className="nes-text is-success">Hi there, I'm Theo!</h1>
+        <p>
+          Just another CS-related graduate trying his absolute best. My journey
+          in FullStack Development began roughly in 2023 through academic
+          projects and has evolved into something I am really proud of.
+        </p>
+        <p>
+          When I am not coding, you will probably find me on some camping site
+          or a hopeless indie bar. My friends call me a chameleon when it comes
+          to my entertainment.
+        </p>
+        <p>
+          My goal is to broaden my curiosity about new technologies and never,
+          ever settle down, always pushing myself to learn, build and explore
+          the unknown!
+        </p>
+      </div>
+
+      {/* Right side - avatar */}
+      <div className="about-photo">
+        {/* Replace src with your image later */}
+        <img
+          src={photoprofile}
+          alt=""
+          className="avatar"
+        />
+      </div>
+    </div>
       <section style={{ marginTop: "2rem" }}>
         <i className="nes-icon coin is-medium"></i>
         <i className="nes-icon heart is-medium"></i>
